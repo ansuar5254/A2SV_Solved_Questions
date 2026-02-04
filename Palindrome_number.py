5e082f427e54df8db2b92be1aca524eee312ed45
ansuar5254 <ansuar5254@gmail.com>
2026-02-04 11:04:29 +0300

    Implement palindrome checking for integers

---
+++ Palindrome_number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :z: bool
+        """
+     
+        if x < 0:
+           return False
+        original, reversed_num = x, 0
+        while x > 0:
+           digit = x % 10
+           reversed_num = reversed_num * 10 + digit
+           x //= 10
+        return original == reversed_num
+
+
+        
+
+        
