715b4a078510178708d29c5a7544d8d2e2a833c2
ansuar5254 <ansuar5254@gmail.com>
2026-02-11 08:01:49 +0300

    Create LongestConsecutiveSequence.py

---
+++ LongestConsecutiveSequence.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+        max_len = 0
+        for num in nums_set:
+            if num - 1 not in nums_set:
+                count = 1
+                start = num
+                while start + 1 in nums_set:
+                    count += 1
+                    start += 1
+                max_len = max(max_len,count)
+        return max_len
+            
+                 
+
+                
+     
+        
+
+
+        
