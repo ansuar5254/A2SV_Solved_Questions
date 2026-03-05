c86bf23ab2e2eedad559fd48a817337ec88c19cd
ansuar5254 <ansuar5254@gmail.com>
2026-03-05 14:21:45 +0300

    Time: 31 ms (76.86%), Space: 22.3 MB (20.04%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        checker = {0:1}
+        count = 0
+        pre_sum = list(accumulate(nums))
+        for num in pre_sum:
+            if (num - k) in checker:
+                count += checker[num-k]
+            if num in checker:   
+                 checker[num] += 1 
+            else:
+                checker[num] = 1
+
+        return count
+    
+      
+            
+
+        
+        
+
