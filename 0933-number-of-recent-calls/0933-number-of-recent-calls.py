c511c290f33eec731322b0775aaac3f0c067c8b7
ansuar5254 <ansuar5254@gmail.com>
2026-03-10 16:11:18 +0300

    Time: 18 ms (98.14%), Space: 25.5 MB (10.44%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -2,13 +2,13 @@ class RecentCounter:
 
     def __init__(self):
         self.arr = []
+        self.l = 0
     def ping(self, t: int) -> int:
         self.arr.append(t)
-        l = 0
-        while self.arr and t - 3000 > self.arr[l]:
-            l += 1
-        return len(self.arr) - l
-        
+        while self.arr and t - 3000 > self.arr[self.l]:
+            self.l += 1
+        return len(self.arr) - self.l
+
         
 
 
