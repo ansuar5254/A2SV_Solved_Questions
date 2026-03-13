9bd4acb52f228dd90e36f7be4af91d99900d3d0d
ansuar5254 <ansuar5254@gmail.com>
2026-03-13 11:21:14 +0300

    Add solution for Daily Temperatures

---
+++ leetcode/0739-Daily-Temperatures/0739-Daily-Temperatures.py
@@ -0,0 +1,25 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        q = deque()
+        n = len(temperatures)
+        ans = []
+        
+        for i in range(n-1,-1,-1):
+            count = 1
+            if not q:
+                ans.append(0)
+
+            else:
+                while q:
+                    if temperatures[i] >= q[0][0]:
+                        x,y = q.popleft()
+                        count += y
+                    else:
+                        break
+                if not q:
+                    ans.append(0)
+                else:
+                    ans.append(count)
+            q.appendleft([temperatures[i],count])
+        ans.reverse()
+        return ans
