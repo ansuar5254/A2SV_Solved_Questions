bce4bf0a1f95a931c53c827604ddd86af5994326
ansuar5254 <ansuar5254@gmail.com>
2026-03-13 17:04:16 +0300

    Time: 0 ms (100%), Space: 19.4 MB (33.16%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -0,0 +1,27 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        index = []
+        diff = []
+        for i in range(len(costs)):
+            diff.append([costs[i][1]-costs[i][0],i])
+
+        diff.sort()
+        for val,ind in diff:
+            index.append(ind)
+
+
+        l = 0
+        r = len(costs)-1
+        total = 0
+        while l < r:
+            total += costs[index[l]][1]
+            total += costs[index[r]][0]
+            l += 1
+            r -= 1
+        return total
+      
+            
+
+
+
+            
