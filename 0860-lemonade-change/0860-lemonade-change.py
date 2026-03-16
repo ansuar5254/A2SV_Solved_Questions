c31f8734cf2efa96983070d80e8332ae7b48e2a3
ansuar5254 <ansuar5254@gmail.com>
2026-03-16 07:22:33 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,35 +1,42 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        pre_sum = 0
-        change = defaultdict(int)
+        five = 0
+        ten = 0
         for i in range(len(bills)):
-            chan = bills[i] - 5
-            if chan and chan in change:
-                change[chan] -= 1
-                if change[chan] == 0:
-                    del change[chan]
-            elif chan == 15 and 10 in change and 5 in change:
-                change[10] -= 1
-                if change[10] == 0:
-                    del change[10]
-                change[5] -= 1
-                if change[5] == 0:
-                    del change[5]
+            change = bills[i]-5
+            if change == 15:
+                if five >= 1 and ten >= 1:
+                    five -= 1
+                    ten -= 1
+                elif five >= 3:
+                    five -=  3
+                else:
+                    return False
+            if change == 10:
+                if ten >= 1:
+                    ten -= 1
+                elif five >= 2:
+                    five -= 2
+                else:
+                    return False
+            if change == 5:
+                if five >= 1:
+                    five -= 1
+                else:
+                    return False
+            if bills[i] == 5:
+                five += 1
 
-            elif chan == 15 and 5 in change and change[5] >= 3:
-                change[5] -= 3
-                if change[5] == 0:
-                    del change[5]
+            elif bills[i] == 10:
+                ten += 1
+        return True
+            
 
-            elif chan == 10 and 5 in change and change[5] >= 2:
-                change[5] -= 2
-                if change[5] == 0:
-                    del change[5]
-            elif chan != 0:
-                return False
 
-            change[bills[i]] += 1
+            
 
+      
+    
         return True
 
         
