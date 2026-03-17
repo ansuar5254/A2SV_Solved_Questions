07de4f451bad5c435ab0fb2c16f7ba949c6e47a5
ansuar5254 <ansuar5254@gmail.com>
2026-03-17 16:32:33 +0300

    Add solution for Lemonade Change

---
+++ leetcode/0860-Lemonade-Change/0860-Lemonade-Change.py
@@ -0,0 +1,32 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five = 0
+        ten = 0
+        for i in range(len(bills)):
+            change = bills[i]-5
+            if change == 15:
+                if five >= 1 and ten >= 1:
+                    five -= 1
+                    ten -= 1
+                elif five >= 3:
+                    five -=  3
+                else:
+                    return False
+            if change == 10:
+                if ten >= 1:
+                    ten -= 1
+                elif five >= 2:
+                    five -= 2
+                else:
+                    return False
+            if change == 5:
+                if five >= 1:
+                    five -= 1
+                else:
+                    return False
+            if bills[i] == 5:
+                five += 1
+
+            elif bills[i] == 10:
+                ten += 1
+        return True
