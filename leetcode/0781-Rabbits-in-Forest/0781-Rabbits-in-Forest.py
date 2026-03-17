d5dfaf7501c4361a7c022afc9dd3271b0d70b28e
ansuar5254 <ansuar5254@gmail.com>
2026-03-17 17:19:36 +0300

    Add solution for Rabbits in Forest

---
+++ leetcode/0781-Rabbits-in-Forest/0781-Rabbits-in-Forest.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        count = Counter(answers)
+        rabit = 0
+        for key,val in count.items():
+            same_col = key+1
+            groups = ceil(val/same_col)
+            rabit += (groups*same_col)
+        return rabit
