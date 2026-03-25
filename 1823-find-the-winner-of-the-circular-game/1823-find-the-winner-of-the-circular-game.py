034659cd9787beec4fbc6548b46910bb51b25ceb
ansuar5254 <ansuar5254@gmail.com>
2026-03-26 00:07:56 +0300

    Time: 23 ms (32.75%), Space: 20.4 MB (17.72%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        arr = [i+1 for i in range(n)]
+        def helper(a,m):
+            if m == 1:
+                return a[0]
+            a.remove(a[(k-1)%m])
+            a = a[(k-1)%m:] + a[:(k-1)%m]
+            return helper(a,len(a))
+        return helper(arr,n)
+        
