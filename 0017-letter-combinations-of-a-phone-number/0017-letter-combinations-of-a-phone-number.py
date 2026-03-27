e17af6e833a91fd8c9357892a0c0668188ae672d
ansuar5254 <ansuar5254@gmail.com>
2026-03-27 16:50:30 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -4,18 +4,19 @@ class Solution:
             return []
         
         letter = {
-            "2": "abc", "3": "def", "4": "ghi",
-            "5": "jkl", "6": "mno", "7": "pqrs",
-            "8": "tuv", "9": "wxyz"
+            "2": "abc", "3": "def", "4": "ghi","5": "jkl", "6": "mno", "7": "pqrs","8": "tuv", "9": "wxyz"
         }
-        ans = ['']
-        for d in digits:
-            temp = []
-            for com in ans:
-                for ch in letter[d]:
-                    temp.append(com+ch)
-            print(temp)
-            ans = temp
+        
+        ans = []
+        def helper(i,curr):
+        
+            if i == len(digits):
+                ans.append(curr)
+                return 
+        
+            for ch in letter[digits[i]]: 
+                helper(i+1,curr+ch)
+        helper(0,'')
         return ans
 
         
