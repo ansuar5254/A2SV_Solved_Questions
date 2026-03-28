5a115ba5b7453e5f63cbfe85ca50cad24e05ce2e
ansuar5254 <ansuar5254@gmail.com>
2026-03-28 08:40:41 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -6,18 +6,26 @@ class Solution:
         letter = {
             "2": "abc", "3": "def", "4": "ghi","5": "jkl", "6": "mno", "7": "pqrs","8": "tuv", "9": "wxyz"
         }
-        
         ans = []
+        n = len(digits)
         def helper(i,curr):
-        
-            if i == len(digits):
+            if i == n:
                 ans.append(curr)
                 return 
-        
-            for ch in letter[digits[i]]: 
+
+            for ch in letter[digits[i]]:
+                
                 helper(i+1,curr+ch)
+                
+            
         helper(0,'')
         return ans
+
+
+        
+
+        
+       
 
         
        
