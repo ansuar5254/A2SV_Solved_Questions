75e438738079ce840e0236d5f3eb59e6985d72b7
ansuar5254 <ansuar5254@gmail.com>
2026-04-10 20:52:07 +0300

    Add solution for Maximum Candies Allocated to K Children

---
+++ leetcode/2226-Maximum-Candies-Allocated-to-K-Children/2226-Maximum-Candies-Allocated-to-K-Children.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        self.ans = 0
+        def checker(n):
+            c = 0
+            for cand in candies:
+                c += cand//n
+
+            if c >= k:
+                return True
+            else:
+                return False
+
+        def binarySearch():
+            low,high = 1,max(candies)
+            while low <= high:
+                mid = (low + high)//2
+                if checker(mid):
+                    print(mid)
+                    self.ans = mid
+                    low = mid + 1
+
+                else:
+                    high = mid - 1
+
+        binarySearch()
+        
+        return self.ans
