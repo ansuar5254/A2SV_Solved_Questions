9d2f3b40f5c872e10fdfa450d56a836299c58f4a
ansuar5254 <ansuar5254@gmail.com>
2026-04-10 20:51:43 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
@@ -1,14 +1,10 @@
 class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
-
-        candies.sort()
         self.ans = 0
-
         def checker(n):
             c = 0
             for cand in candies:
                 c += cand//n
-        
 
             if c >= k:
                 return True
