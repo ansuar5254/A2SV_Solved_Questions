de0b589d08c23d4390003d7d8f4581588a58f3f2
ansuar5254 <ansuar5254@gmail.com>
2026-04-11 19:41:51 +0300

    Add solution for Elimination Game

---
+++ leetcode/0390-Elimination-Game/0390-Elimination-Game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lastRemaining(self, n: int) -> int:
+        
+        def helper(head,step,remain,left):
+            if remain == 1:
+                return head
+
+            if left or remain % 2 :
+                head += step
+
+            return helper(head,step*2,remain//2,not left)
+
+        return helper(1,1,n,True)
