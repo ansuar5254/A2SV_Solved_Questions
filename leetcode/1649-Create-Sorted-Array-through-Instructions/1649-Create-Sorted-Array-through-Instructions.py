6921fb3741b198c9a22761ca0164ac5dd8ee8095
ansuar5254 <ansuar5254@gmail.com>
2026-04-17 01:50:37 +0300

    Add solution for Create Sorted Array through Instructions

--- leetcode/1649-Create-Sorted-Array-through-Instructions/1649-Create-Sorted-Array-through-Instructions.py
+++ leetcode/1649-Create-Sorted-Array-through-Instructions/1649-Create-Sorted-Array-through-Instructions.py
@@ -1,38 +1,46 @@
 class Solution:
-    def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
-        arr = []
-        count = [0]
-        n = len(nums1)
-        for i in range(n):
-            arr.append(nums1[i]-nums2[i])
-        def merged(left_arr,right_arr):
+    def createSortedArray(self, instructions: List[int]) -> int:
+        left_c = [0] * len(instructions)
+        right_c = [0] * len(instructions)
+        def merged(left_arr, right_arr):
             l = 0
             r = 0
             merge = []
             while l < len(left_arr) and r < len(right_arr):
-                if left_arr[l] <= right_arr[r]:
+                if left_arr[l][0] <= right_arr[r][0]:
                     merge.append(left_arr[l])
                     l += 1
                 else:
                     merge.append(right_arr[r])
                     r += 1
-
             merge.extend(left_arr[l:])
             merge.extend(right_arr[r:])
             return merge
-
-        def divided(l,r):
+        def divided(l, r):
             if l == r:
-                return [arr[l]]
+                return [[instructions[l], l]]
+            m = (l + r) // 2
+            left = divided(l, m)
+            right = divided(m + 1, r)
+            lp = 0
+            k = 0
+            for val, i in right:
+                while lp < len(left) and left[lp][0] < val:
+                    lp += 1
+                less = lp
 
-            m = (l + r)//2
-            left = divided(l,m)
-            right = divided(m+1,r)
-            for i in range(len(left)):
-                val = left[i] - diff
-                ind = bisect_left(right,val)
-                ind = len(right)-ind
-                count[0] += ind
-            return merged(left,right)
-        divided(0,len(arr)-1)
-        return count[0]
+                if k < lp:
+                    k = lp
+                    
+                while k < len(left) and left[k][0] == val:
+                    k += 1
+                left_c[i] += less
+                right_c[i] += (len(left) - k)
+            return merged(left, right)
+        divided(0, len(instructions) - 1)
+        cost = 0
+        mod = 10**9 + 7
+        for i in range(len(left_c)):
+            cost += min(left_c[i], right_c[i])
+
+        return cost % mod
