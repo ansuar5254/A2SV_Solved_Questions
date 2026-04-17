2c69cb4910ca1e371ad27ffab825a7692336acb6
ansuar5254 <ansuar5254@gmail.com>
2026-04-17 17:59:57 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,29 +1,17 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        return nums[len(nums)-k]
-        # l = 0
-        # r = len(nums)-1
-        # t = len(nums) - k
-      
-        # while l <= r:
-        #     p = random.randint(l,r)
-        #     w = l+1
-           
-        #     for i in range(w,r+1):
-        #         if nums[i] < nums[p]: 
-        #             nums[i],nums[w] = nums[w],nums[i]
-        #             w += 1
-            
-        #     p = w-1
-        #     nums[p],nums[l] = nums[l],nums[p]
+        count = Counter(nums)
+        large = max(nums)
+        while k > 1:
+            while large in count:
+                k -= 1
+                count[large] -= 1
+                if count[large] == 0:
+                    del count[large]
+
+
+                
+            large -= 1
+        return large
         
-        #     if  p == t:
-        #         return nums[p]
-
-        #     elif p < t:
-        #         l = p+1
-        #     else:
-        #         r = p-1
-
-             
+            
