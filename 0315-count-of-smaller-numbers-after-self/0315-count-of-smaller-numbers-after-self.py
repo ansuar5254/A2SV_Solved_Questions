12bf386223dcb8dd3ab7df9bd23bef365f10798d
ansuar5254 <ansuar5254@gmail.com>
2026-04-19 07:16:59 +0300

    Time: 1542 ms (31.42%), Space: 56.9 MB (18.31%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,41 +1,43 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
+        count = [0] * len(nums)
         def merge(left_arr,right_arr):
-            l = 0
-            r = 0
-            ans = []
+            l ,r = 0,0
+            merged = []
             while l < len(left_arr) and r < len(right_arr):
                 if left_arr[l][0] <= right_arr[r][0]:
-                    ans.append(left_arr[l])
+                    merged.append(left_arr[l])
                     l += 1
                 else:
-                    ans.append(right_arr[r])
+                    merged.append(right_arr[r])
                     r += 1
 
-            ans.extend(left_arr[l:])
-            ans.extend(right_arr[r:])
-          
-            return ans
-
-        def divided(final,left,right):
+            merged.extend(right_arr[r:])
+            merged.extend(left_arr[l:])
+            return merged
+        def divided(left,right):
             if left == right:
                 return [[nums[left],left]]
 
-            mid =( left+right)//2
-            left_half = divided(final,left,mid)
-            right_half = divided(final,mid+1,right)
-            for a,b in left_half:
-                ind = bisect.bisect_left(right_half,a,key = lambda x : x[0])
-                final[b] += ind
+            mid =( left + right)//2
+            left_half = divided(left,mid)
+            right_half = divided(mid+1,right)
+            j = 0
+            for i in range(len(left_half)):
+                while j < len(right_half) and left_half[i][0] > right_half[j][0]:
+                    j += 1
+                count[left_half[i][1]] += j
+                
+            
 
             return merge(left_half,right_half)
-        f = [0] * len(nums)
-        divided(f,0,len(nums)-1)
-        return f
+        divided(0,len(nums)-1)
+        return count
+    
+        
+
+                
 
 
 
-
-
-
-                    
+        
