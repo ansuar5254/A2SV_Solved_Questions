ee16942ccef0d1d6f0ef8757dd6f1f22faecc481
ansuar5254 <ansuar5254@gmail.com>
2026-04-21 04:05:29 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,28 +1,32 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         visited = [0]*len(graph)
-        def dfs(node):
-            for neigh in graph[node]:
-                if not visited[neigh]:
-                    if visited[node] == 1:
-                        visited[neigh] = 2
-                    else:
-                        visited[neigh] = 1
-                    if not dfs(neigh):
-                        return False
-
-                else:
-                    if visited[neigh] == visited[node]:
-                        return False 
-            return True
+        stack = []
 
         for i in range(len(graph)):
             if not visited[i]:
                 visited[i] = 1
-                if not dfs(i):
-                    return False
+                stack.append(i)
+                while stack:
+                    node = stack.pop()
+                    for neigh in graph[node]:
+                        if not visited[neigh]:
+                            stack.append(neigh)
+                            if visited[node] == 1:
+                                visited[neigh] = 2
+                            else:
+                                visited[neigh] = 1
+                        else:
+                            print(node,visited[node],neigh,visited[neigh])
+                            if visited[node] == visited[neigh]:
+                                return False
 
         return True
+                    
+
+
+      
+
         
 
 
