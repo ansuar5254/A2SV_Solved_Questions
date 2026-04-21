f7a89965cd94a378b7f253de6c05e9c4afe3edfc
ansuar5254 <ansuar5254@gmail.com>
2026-04-21 03:09:46 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,22 +2,28 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
         direction = [(0,1),(0,-1),(1,0),(-1,0)] 
-        island = 0           
-        def dfs(row,col):
-            if row < 0 or col < 0  or row >= len(grid) or col >= len(grid[row]) or grid[row][col] == '0':
-                return 
-
-            grid[row][col] = '0'
-            for x,y in direction:
-                new_r = row + x
-                new_c = col + y
-                dfs(new_r,new_c)
+        island = 0 
+        stack = []          
+        
 
         for r in range(len(grid)):
             for c in range(len(grid[r])):
                 if grid[r][c] == '1':    
                     island += 1
-                    dfs(r,c)
+
+                stack.append((r,c))
+                while stack:
+                    row,col = stack.pop()
+
+                    if row < 0 or col < 0  or row >= len(grid) or col >= len(grid[row]) or grid[row][col] == '0':
+                        continue
+
+                    grid[row][col] = '0'
+                    for x,y in direction:
+                        new_r = row + x
+                        new_c = col + y
+                        stack.append((new_r,new_c))
+
         return island
 
                 
