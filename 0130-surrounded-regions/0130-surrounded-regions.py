0f407306a404fd646a40c3fdd5a4a4c547513e9b
ansuar5254 <ansuar5254@gmail.com>
2026-04-21 19:32:31 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -13,36 +13,53 @@ class Solution:
                 return True
             return False
 
-        def isb(r,c):
-            for x,y in direction:
-                new_r = r + x 
-                new_c = c + y
-                if 0 <= new_r < len(board) and 0 <= new_c < len(board[r]):
-                    continue
-                else:
-                    return False
-            return True
+        
 
         not_sr = []
+        def dfs():  
+            while stack:
+                ro,co = stack.pop()
+                for x,y in direction:
+                    new_r = ro + x
+                    new_c = co + y
+                    if isbound(new_r,new_c) and board[new_r][new_c] == 'O' and visited[new_r][new_c] == 0:
+                        stack.append((new_r,new_c))
+                        visited[new_r][new_c] = 1
+                        not_sr.append((new_r,new_c))
 
+        for c in range(m):
+            if board[0][c] == 'O':
+                stack.append((0,c))
+                visited[0][c] = 1
+                not_sr.append((0,c))
+                dfs()
+
+        for r in range(1, n):
+            if board[r][m - 1] == 'O':
+                stack.append((r,m-1))
+                visited[r][m-1] = 1
+                not_sr.append((r,m-1))
+                dfs()
+              
+
+        for c in range(m - 2, -1, -1):
+            if board[n - 1][c] == 'O':
+                stack.append((n-1,c))
+                visited[n-1][c] = 1
+                not_sr.append((n-1,c))
+                dfs()
+                
+
+        for r in range(n - 2, 0, -1):
+            if board[r][0] == 'O':
+                stack.append((r,0))
+                visited[r][0] = 1
+                not_sr.append((r,0))
+                dfs()
+                
+                   
         for r in range(n):
             for c in range(m):
-                if board[r][c] == 'O' and not isb(r,c):
-                    stack.append((r,c))
-                    not_sr.append((r,c))
-                    visited[r][c] = 1
-                    while stack:
-                        ro,co = stack.pop()
-                        for x,y in direction:
-                            new_r = ro + x
-                            new_c = co + y
-                            if isbound(new_r,new_c) and board[new_r][new_c] == 'O' and visited[new_r][new_c] == 0:
-                                stack.append((new_r,new_c))
-                                visited[new_r][new_c] = 1
-                                not_sr.append((new_r,new_c))
-
-        for r in range(len(board)):
-            for c in range(len(board[r])):
                 board[r][c] = 'X'
 
         for r,c in not_sr:
