a05f745d942f1bef15aba02a0e65926181d01621
ansuar5254 <ansuar5254@gmail.com>
2026-04-22 16:42:22 +0300

    Time: 239 ms (72.29%), Space: 21.4 MB (98.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        direction = [(0,1),(0,-1),(1,0),(-1,0)]
+        n = len(grid)
+        m = len(grid[0])
+        stack = []
+        island = 0
+        def isbound(r,c):
+           if  0 <= r < n and 0 <= c < m:
+                return True
 
-        direction = [(0,1),(0,-1),(1,0),(-1,0)] 
-        island = 0 
-        stack = []          
-        
-
-        for r in range(len(grid)):
-            for c in range(len(grid[r])):
-                if grid[r][c] == '1':    
+        for r in range(n):
+            for c in range(m):
+                if grid[r][c] == '1':
                     island += 1
-
-                stack.append((r,c))
-                while stack:
-                    row,col = stack.pop()
-
-                    if row < 0 or col < 0  or row >= len(grid) or col >= len(grid[row]) or grid[row][col] == '0':
-                        continue
-
-                    grid[row][col] = '0'
-                    for x,y in direction:
-                        new_r = row + x
-                        new_c = col + y
-                        stack.append((new_r,new_c))
-
+                    grid[r][c] = '0'
+                    stack.append((r,c))
+                    while stack:
+                        ro,co = stack.pop()
+                        for x,y in direction:
+                            new_r = ro + x
+                            new_c = co + y
+                            if isbound(new_r,new_c) and grid[new_r][new_c] =='1':
+                                stack.append((new_r,new_c))
+                                grid[new_r][new_c] = '0'
         return island
 
-                
+                                
