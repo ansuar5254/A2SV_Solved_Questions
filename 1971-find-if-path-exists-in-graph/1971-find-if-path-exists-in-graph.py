f4d06bc2771cbe8fb51618e728ca379e81b545f6
ansuar5254 <ansuar5254@gmail.com>
2026-07-22 02:10:24 -0700

    Time: 289 ms (67.81%), Space: 85.1 MB (84.34%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,25 +1,29 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        # this is the iterative approach
+        
         graph = [[] for _ in range(n)]
+
         for u,v in edges:
             graph[u].append(v)
             graph[v].append(u)
 
-        visited = [0]*n
+        visited = set()
         stack = [source]
-        visited[source] = 1
+        visited.add(source)
         while stack:
             node = stack.pop()
             if node == destination:
                 return True
-
             for neigh in graph[node]:
-                if visited[neigh] == 0: 
-                    stack.append(neigh)
-                    visited[neigh] = 1
+                if neigh not in visited:
+                   stack.append(neigh)
+                   visited.add(neigh)
+
         return False
+                   
+
+    
 
 
-        
-        
         
